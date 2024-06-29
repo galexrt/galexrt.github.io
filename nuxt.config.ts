be28c3ccc836a6a57a118bf3baae20b24ef8669c
Alexander Trost <galexrt@googlemail.com>
2024-06-29 16:30:31 +0200

    fix: add phosphor icons to nuxt ui config
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
         },
     },
     ui: {
-        icons: ['heroicons', 'simple-icons'],
+        icons: ['heroicons', 'ph', 'simple-icons'],
         safelistColors: ['blue-violet'],
     },
     mdc: {
